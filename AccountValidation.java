c36f200dd7f06199a456c01dd32a7fa5e5dc6f0f
MilasBogdan <milas.bogdan@yahoo.com>
2017-04-19 16:19:36 +0300

    Delete AccountValidation.java

--- AccountValidation.java
+++
@@ -1,9 +0,0 @@
-package Business;
-
-public class AccountValidation {
-
-	public static String checkAccountData(int accounID , String type , long Amount ,String date){
-		if (Amount<0) return "fonduri insuficiente";
-		return "Fonduri suficiente";
-	}
-}

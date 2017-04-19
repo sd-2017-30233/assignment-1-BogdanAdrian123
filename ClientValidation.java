182cd70118c8248de3f1108a8be3e7b581b8deba
MilasBogdan <milas.bogdan@yahoo.com>
2017-04-19 16:19:49 +0300

    Delete ClientValidation.java

--- ClientValidation.java
+++
@@ -1,9 +0,0 @@
-package Business;
-
-public class ClientValidation {
-	public static String validateClientData(String name , int ICN, long CNP ,String address){
-		if (CNP>=1000000000000L && CNP<=9999999999999L) return "Client inexistent";
-		else return "Correct";
-	}
-
-}
